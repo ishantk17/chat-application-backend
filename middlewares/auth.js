b056d6e342d86a9120d5cb7d302f7f299a475424
ishantk17 <ishantkumar85627@gmail.com>
2024-01-02 00:55:49 +0530

    second

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,37 +1,27 @@
-const { default: mongoose } = require('mongoose')
-const Users = mongoose.model('users');
-const jwt = require('jsonwebtoken')
+const { default: mongoose } = require("mongoose");
+const Users = mongoose.model("users");
+const jwt = require("jsonwebtoken");
 
-const Auth = async (req, res,next) => {
-    console.log('inside auth')
-    try {
-        const header = req.headers['authorization']
-        const token = header && header.split(' ')[1]
-        console.log(token)
-        if (token == null) {
-            return res.status(400).json({ msg: "unauthorized" })
-        }
-        console.log("11")
-        const verifyToken = jwt.verify(token, process.env.JWT_SECRET)
-        console.log("verifytoken",verifyToken)
-        if (verifyToken) {
-            const user = await Users.findOne({ email: verifyToken._id })
-            if (!user) {
-                return res.status(400).json({ msg: "verification Failed" })
-            }
-            console.log("1")
-            req.user = user
-            next()
-        } else {
-            return res.status(401).json({ msg: "unauthorized" })
-        }
-    } catch (error) {
-        console.log("2")
-        return res.status(400).json({ err: error.message })
-
+const Auth = async (req, res, next) => {
+  try {
+    const header = req.headers["authorization"];
+    const token = header && header.split(" ")[1];
+    if (token == null) {
+      return res.status(400).json({ msg: "unauthorized" });
     }
-
-
-}
-
-module.exports = { Auth }
+    const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
+    if (verifyToken) {
+      const user = await Users.findOne({ email: verifyToken._id });
+      if (!user) {
+        return res.status(400).json({ msg: "verification Failed" });
+      }
+      req.user = user;
+      next();
+    } else {
+      return res.status(401).json({ msg: "unauthorized" });
+    }
+  } catch (error) {
+    return res.status(400).json({ err: error.message });
+  }
+};
+module.exports = { Auth };
